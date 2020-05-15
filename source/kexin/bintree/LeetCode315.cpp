493443956c7fe6d646f76dd95865786033e7a236
zhaohailong <zhao.hailong@outlook.com>
2020-05-16 03:51:34 +0800

    LeetCode315

--- source/kexin/bintree/LeetCode315.cpp
+++ source/kexin/bintree/LeetCode315.cpp
@@ -21,13 +21,90 @@
  */
 
 #include <vector>
+#include <iostream>
 
 using namespace std;
+
+struct TreeNode2 {
+    int val;
+    TreeNode2 *left;
+    TreeNode2 *right;
+    int leftCount;
+
+    TreeNode2(int x) : val(x), left(nullptr), right(nullptr), leftCount(0)
+    {}
+};
 
 class Solution {
 public:
     vector<int> countSmaller(vector<int> &nums)
     {
+        vector<int> ret;
+        if (nums.size() < 1) {
+            return ret;
+        }
+        vector<TreeNode2 *> nodeVec;
+        for (int i = nums.size() - 1; i >= 0; i--){
+            nodeVec.push_back(new TreeNode2(nums[i]));
+        }
+        int countSmall = 0;
+        ret.push_back(countSmall);
+        for (int i = 1; i < nodeVec.size(); i++) {
+            countSmall = 0;
+            InsertNode(nodeVec[0], nodeVec[i], countSmall);
+            ret.push_back(countSmall);
+        }
+        vector<int> reverseRet;
+        ReverseArray(ret, reverseRet);
+        // delete new:that's why use nodeVec
+        for (int i = 0; i < nodeVec.size(); i++) {
+            delete nodeVec[i];
+            nodeVec[i] = nullptr;
+        }
+        return reverseRet;
+    }
 
+    // 向 node 二叉查找树里插入结点 insertnode，并更新左子树数量，同时获取当前结点小于插入结点的数量
+    TreeNode2 *InsertNode(TreeNode2 *node, TreeNode2 *insertnode, int &countSmall)
+    {
+        if (node == nullptr) {
+            return insertnode;
+        }
+        if (insertnode->val > node->val) {
+            countSmall += node->leftCount + 1;
+            node->right = InsertNode(node->right, insertnode, countSmall);
+        } else if (insertnode->val <= insertnode->val) {
+            node->leftCount++;
+            node->left = InsertNode(node->left, insertnode, countSmall);
+        }
+        return node;
+    }
+
+    // 逆序数组
+    void ReverseArray(vector<int> &nums, vector<int> &ret)
+    {
+        for (int i = nums.size() - 1; i >= 0; i--) {
+            ret.push_back(nums[i]);
+        }
     }
 };
+
+int main()
+{
+    vector<int> a = {5, 2, 6, 1};
+    vector<int> a2;
+    Solution s;
+    s.ReverseArray(a, a2);
+    cout << a2.size() << endl;
+    cout << a2[0] << endl;
+    cout << a2[1] << endl;
+    cout << a2[2] << endl;
+    //
+    cout << "test countSmaller " << endl;
+    vector<int> ret = s.countSmaller(a);
+    cout << ret.size() << endl;
+    cout << ret[0] << endl;
+    cout << ret[1] << endl;
+    cout << ret[2] << endl;
+    cout << ret[3] << endl;
+}
